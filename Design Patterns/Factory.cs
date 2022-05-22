8aeffab73e32ed6d51f43b065b9551ba95785bd9
Seyoon Park <psyoona@naver.com>
2022-05-22 18:05:04 +0900

    Add Factory design pattern

---
+++ Design Patterns/Factory.cs
@@ -0,0 +1,57 @@
+﻿namespace DesignPattern.Design_Patterns
+{
+	using System;
+
+	public class Factory
+	{
+		ILogger logger = LoggerFacotry.Create("DB");
+		// ...
+	}
+
+	public interface ILogger
+	{
+
+	}
+
+	public class LoggerFacotry
+	{
+		// Static Factory Method
+		// (loggerType을 enum으로 하는 것이 일반적)
+		public static ILogger Create(string loggerType)
+		{
+			ILogger logger = null;
+
+			switch (loggerType)
+			{
+				case "DB":
+					logger = new DbLogger();
+					break;
+				case "XML":
+					logger = new XmlLogger();
+					break;
+				case "JSON":
+					logger = new JsonLogger();
+					break;
+				default:
+					throw new InvalidOperationException();
+			}
+
+			return logger;
+		}
+
+		public class DbLogger : ILogger
+		{
+		}
+
+		public class XmlLogger : ILogger
+		{
+
+		}
+
+		public class JsonLogger : ILogger
+		{
+
+		}
+	}
+
+}
